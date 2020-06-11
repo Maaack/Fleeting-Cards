f32476127bbea8cdd30372aaf46330134aee5e6d
Maaack <marek.belski@gmail.com>
2020-06-10 17:56:46 -0700

    CARD-01: Flip cards and tween motions.

--- Scenes/Card/Card.gd
+++ Scenes/Card/Card.gd
@@ -11,9 +11,15 @@ signal drop
 
 onready var outline_node = $CardMesh/OutlineMesh
 onready var body_node = $KinematicBody
+onready var tween_node = $Tween
 
 var focused: bool = false
 var dragging: bool = false
+var flipped: bool = false
+var initial_rotation:Vector3
+
+func ready():
+	initial_rotation = rotation
 
 func _on_KinematicBody_mouse_entered():
 	focus()
@@ -24,10 +30,16 @@ func _on_KinematicBody_mouse_exited():
 func _on_KinematicBody_input_event(camera, event, click_position, click_normal, shape_idx):
 	emit_signal("mouse_over", self, camera, event, click_position, click_normal, shape_idx)
 	if event is InputEventMouseButton:
-		if event.pressed:
-			drag()
-		else:
-			drop()
+		if event.button_index == 1:
+			if event.pressed:
+				drag()
+				if event.doubleclick:
+					flip()
+			else: 
+				drop()
+		if event.button_index == 2:
+			if event.pressed:
+				flip()
 
 func focus():
 	focused = true
@@ -48,3 +60,16 @@ func drop():
 	dragging = false
 	body_node.input_ray_pickable = true
 	emit_signal('drop', self)
+
+func flip():
+	if flipped:
+		tween_node.interpolate_property(self, "rotation", rotation, initial_rotation, get_flip_time())
+	else:
+		var flipped_rotation = initial_rotation
+		flipped_rotation.z += PI
+		tween_node.interpolate_property(self, "rotation", rotation, flipped_rotation, get_flip_time())
+	flipped = !(flipped)
+	tween_node.start()
+
+func get_flip_time():
+	return 0.25
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -32,17 +32,20 @@ func _on_Card_drag(card:Card):
 
 func _on_Card_drop(card:Card):
 	if is_instance_valid(dragging):
+		var final_translation:Vector3 = Vector3()
 		if hovering is Table:
-			dragging.translation.y = hovering.translation.y
+			final_translation = dragging.translation * VECTOR_3_MASK
 		if hovering is Card:
-			dragging.translation.y = hovering.translation.y + 0.04
+			final_translation = hovering.translation + Vector3(0.0, 0.1, 0.0)
+		dragging.tween_node.interpolate_property(dragging, "translation", dragging.translation, final_translation, _get_tween_time())
+		dragging.tween_node.start()
 		dragging = null
-
-func _on_Table_mouse_over(table:Table, camera, event, click_position, click_normal, shape_idx):
-	_hover_over(table, click_position)
 
 func _on_Card_mouse_over(card:Card, camera, event, click_position, click_normal, shape_idx):
 	_hover_over(card)
+
+func _on_Table_mouse_over(table:Table, camera, event, click_position, click_normal, shape_idx):
+	_hover_over(table, click_position)
 
 func _hover_over(spatial:Spatial, click_position:Vector3 = Vector3()):
 	hovering = spatial
@@ -50,4 +53,11 @@ func _hover_over(spatial:Spatial, click_position:Vector3 = Vector3()):
 		var final_translation : Vector3 = spatial.translation + click_position
 		final_translation *= VECTOR_3_MASK
 		final_translation += VECTOR_3_OFFSET
-		dragging.translation = final_translation
+		if dragging.translation.distance_to(final_translation) > 0.5:
+			dragging.tween_node.interpolate_property(dragging, "translation", dragging.translation, final_translation, _get_tween_time())
+			dragging.tween_node.start()
+		else:
+			dragging.translation = final_translation
+
+func _get_tween_time():
+	return 0.1
