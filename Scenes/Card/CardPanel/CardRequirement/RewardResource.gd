c58f31770b33eb063d7fb543f565f6d0e3ba8767
Maaack <marek.belski@gmail.com>
2020-06-26 15:44:18 -0700

    GAME-02: Opportunities become seized opportunities when the initiative is paid. Starting framework for rewards being paid out for meeting requirements.

---
+++ Scenes/Card/CardPanel/CardRequirement/RewardResource.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+
+class_name Reward
+
+export(String) var card_group
+export(PackedScene) var card_scene
--- Scenes/Card/Opportunity/OpportunityCard.gd
+++ Scenes/Card/Opportunity/OpportunityCard.gd
@@ -11,8 +11,10 @@ onready var requirements_node = $Viewport/CardFrontContents/Requirements/Require
 onready var requirements_animation_node = $Viewport/CardFrontContents/Requirements/AnimationPlayer
 
 export(Array, Resource) var init_requirements
+export(Array, Resource) var init_rewards
 
 var requirements : Array = []
+var rewards : Array = []
 
 func _ready():
 	requirements_node.show()
@@ -20,7 +22,10 @@ func _ready():
 		var requirement = init_requirement.duplicate()
 		requirements.append(requirement)
 		requirements_node.add_requirement(requirement)
-
+	for init_reward in init_rewards:
+		var reward = init_reward.duplicate()
+		rewards.append(reward)
+	
 func advance_turn():
 	var advance_return = .advance_turn()
 	if card_to_end <= 0:
@@ -57,5 +62,23 @@ func stack_card(to_stack:Card):
 		if to_stack.is_in_group(requirement.card_group):
 			requirement.card_quantity -= 1
 			to_stack.remove_self()
+			activate_reward()
 			return get_over_card_translation()
 	return .stack_card(to_stack)
+
+func check_requirements():
+	for requirement in requirements:
+		if requirement.card_quantity > 0:
+			return false
+	return true
+
+func _activate_reward():
+	for reward in rewards:
+		if reward is Reward:
+			var card_instance = reward.card_scene.instance()
+			emit_signal("spawn_card", card_instance, stack_card(card_instance))
+
+func activate_reward():
+	if check_requirements():
+		_activate_reward()
+		remove_self()
