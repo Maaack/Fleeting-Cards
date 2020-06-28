f3a3f72e29c5374217abe29bfc220f5548b47f72
Maaack <marek.belski@gmail.com>
2020-06-27 17:20:46 -0700

    GAME-02: Event cards spawn threats and opportunities.

---
+++ Scenes/Card/CardPanel/CardRequirement/SpawnTimerResource.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+
+class_name SpawnTimer
+
+export(int) var card_spawn_delay
+export(PackedScene) var card_scene
---
+++ Scenes/Card/Event/EventCard.gd
@@ -0,0 +1,29 @@
+tool
+extends StackableCard
+
+
+signal spawn_card
+
+export(Array, Resource) var init_spawn_timers
+
+var spawn_timers : Array = []
+
+func _ready():
+	_update_spawn_timers()
+
+func _update_spawn_timers():
+	for init_spawn_timer in init_spawn_timers:
+		var spawn_timer = init_spawn_timer.duplicate()
+		spawn_timers.append(spawn_timer)
+
+func advance_turn():
+	.advance_turn()
+	_check_spawn_timers()
+
+func _check_spawn_timers():
+	for spawn_timer in spawn_timers:
+		if spawn_timer is SpawnTimer:
+			if card_from_start % spawn_timer.card_spawn_delay == 0:
+				var card_instance = spawn_timer.card_scene.instance()
+				emit_signal("spawn_card", card_instance, stack_card(card_instance))
+
